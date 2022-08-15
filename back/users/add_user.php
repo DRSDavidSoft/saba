1c19ab43bb07487e56b9220ba1a04efd7e91b173
David Refoua <David@Refoua.me>
2022-08-15 15:34:04 +0430

    refactor login & add_user; use return instead of else branch

--- back/users/add_user.php
+++ back/users/add_user.php
@@ -1,40 +1,51 @@
 <?php
-include_once ('../inc/functions.php');
+include_once __DIR__ . '/../inc/functions.php';
 
-$response = [
-    'status' => false,
-    'message' => 'Request method must be as "POST"',
-    'result' => null
-];
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (!isset($_POST['username']) or !isset($_POST['password'])) {
+function register()
+{
+    $response = [
+        'status' => false,
+        'message' => 'Request method must be as "POST"',
+        'result' => null
+    ];
+    
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') 
+        return $response;
+    
+    if ( !isset($_POST['username']) or !isset($_POST['password'])) {
         $response['message'] = 'Username or Password is not provided properly.';
-    } else {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $check = get_record_by_field('users', 'username', $username);
-        if (isset($check['username'])) {
-            $response['message'] = 'Username is already exists.';
-        } else {
-            $result = insert_single_record('users', [
-                'username' => $username,
-                'password' => $password
-            ]);
-            if (isset($result['id'])) {
-                $response['status'] = true;
-                $response['message'] = 'User created successfully.';
-                $response['result'] = [
-                    'username' => $username,
-                    'user_id' => $result['id']
-                ];
-            } else {
-                $response['message'] = 'Error while creating user.';
-            }
-        }
+        return $response;
     }
+
+    $username = &$_POST['username'];
+    $password = &$_POST['password'];
+    $check = get_record_by_field('users', 'username', $username);
+    
+    if (isset($check['username'])) {
+        $response['message'] = 'Username is already exists.';
+        return $response;
+    }
+
+    $result = insert_single_record('users', [
+        'username' => $username,
+        'password' => $password
+    ]);
+    if (!isset($result['id'])) {
+        $response['message'] = 'Error while creating user.';
+        return $response;
+    }
+
+    $response['status'] = true;
+    $response['message'] = 'User created successfully.';
+    $response['result'] = [
+        'username' => $username,
+        'user_id' => $result['id']
+    ];
+
+    return $response;
+
 }
 
 header('Content-Type: application/json; charset=utf-8');
-echo json_encode($response);
+echo json_encode(register());
 exit();
--- back/users/login.php
+++ back/users/login.php
@@ -1,36 +1,48 @@
 <?php
-include_once ('../inc/functions.php');
-$response = [
-    'status' => false,
-    'message' => 'Request method must be as "POST"',
-    'result' => null
-];
+include_once __DIR__ . '/../inc/functions.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (!isset($_POST['username']) or  !isset($_POST['password'])) {
+function login()
+{
+    $response = [
+        'status' => false,
+        'message' => 'Request method must be as "POST"',
+        'result' => null
+    ];
+    
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST')
+        return $response;
+    
+    if ( !isset($_POST['username']) or !isset($_POST['password'])) {
         $response['message'] = 'Username or Password is not provided properly.';
-    } else {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $user = get_record_by_field('users', 'username', $username);
-        if ($user and ($user['password'] ?? '') === sha1($password)) {
-            $response['message'] = 'Login successful';
-            $response['status'] = true;
-            $token = createToken(60);
-            $token_life = new DateTime();
-            $token_life->add(DateInterval::createFromDateString(TOKEN_LIFE.' minutes'));
-            $token_life = $token_life->format('Y-m-d H:i:s');
-            update_single_record('users', $user['id'], ['token' => $token, 'token_lifetime' => $token_life]);
-            $response['result'] = [
-                'token' => $token,
-                'lifetime' => $token_life
-            ];
-        } else {
-            $response['message'] = 'Username or Password is not valid.';
-        }
+        return $response;
     }
+
+    $username = &$_POST['username'];
+    $password = &$_POST['password'];
+    $user = get_record_by_field('users', 'username', $username);
+    
+    if ( !$user or ($user['password'] ?? '') !== sha1($password) )
+    {
+        $response['message'] = 'Username or Password is not valid.';
+        return $response;
+    }
+
+    $response['message'] = 'Login successful';
+    $response['status'] = true;
+    $token = createToken(60);
+    $token_life = new DateTime();
+    $token_life->add(DateInterval::createFromDateString(TOKEN_LIFE.' minutes'));
+    $token_life = $token_life->format('Y-m-d H:i:s');
+    update_single_record('users', $user['id'], ['token' => $token, 'token_lifetime' => $token_life]);
+    
+    $response['result'] = [
+        'token' => $token,
+        'lifetime' => $token_life
+    ];
+
+    return $response;
 }
 
 header('Content-Type: application/json; charset=utf-8');
-echo json_encode($response);
+echo json_encode(login());
 exit();
